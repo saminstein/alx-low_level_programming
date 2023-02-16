bed7cc2cf06fcb35e04806d1b59da15c5a1ef590
saminstein <saminstein@yahoo.com>
2023-02-16 06:16:54 -0800

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	fputs(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return (1);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+ return (1);
 }
