e03b8986f92579570e8e7309141cf6368e5ab350
saminstein <saminstein@yahoo.com>
2023-02-18 00:21:31 -0800

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,16 +7,12 @@
 int main(void)
 {
 	char x;
-	char y;
 
 	for (x = 'a'; x <= 'z'; x++)
-	{
 		putchar(x);
-		for (y = 'A'; y <= 'Z'; y++)
-		{
-			putchar(y);
-		}
-	putchar("\n");
+
+	for (x = 'A'; x <= 'Z'; x++)
+		putchar(x);
+	putchar('\n');
 	return (0);
-	}
 }
