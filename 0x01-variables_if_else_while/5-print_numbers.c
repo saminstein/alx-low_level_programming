bb894179a327a0a634fb4413f8d43b9981294686
saminstein <saminstein@yahoo.com>
2023-02-18 01:20:45 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - A c program that prints all single digit numbers
+ * of base 10, from 0 - 9
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
