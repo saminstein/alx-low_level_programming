597ba3fbca409bfb947283bf75658392221c1fc9
saminstein <saminstein@yahoo.com>
2023-02-24 00:49:22 -0800

    8-12

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle -  a function that prints a triangle
+ * @size: size of the triangle
+ */
+void print_triangle(int size)
+{
+	int x, y, z;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = size - x; y > 1; y--)
+			{
+				_putchar(' ');
+			}
+			for (z = 0; z <= x; z++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
