9d4efd7d16b54e23b46f3c06c2952cc1020280b5
saminstein <saminstein@yahoo.com>
2023-03-07 22:40:19 -0800

    0-1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: The string to reverse
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		s++;
+		_print_rev_recursion(s);
+		s--;
+		_putchar(*s);
+	}
+}
