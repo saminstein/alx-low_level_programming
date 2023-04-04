e88fb4b256e7d41398baf5247249cb1dec36ddde
saminstein <saminstein@yahoo.com>
2023-04-04 05:16:34 -0700

    insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx < 1)
 	{
-		printf("invalid position");
+		return (NULL);
 	}
 	for (size = 0; size < idx - 1; size++)
 	{
