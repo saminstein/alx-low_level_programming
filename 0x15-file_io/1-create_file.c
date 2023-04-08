895f08b7e019097da20a59261a309d29f3471672
saminstein <saminstein@yahoo.com>
2023-04-08 04:00:05 -0700

    te

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include "main.h"
 
 
@@ -14,30 +15,13 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd == -1)
 		return (-1);
 
 	if (text_content != NULL)
-		write(fd, text_content, _strlen(text_content));
+		write(fd, text_content, strlen(text_content));
 
 	close(fd);
 	return (1);
-}
-
-/**
- * _strlen - calculates the length of a string
- * @s: string to count
- * Return: length of a string
- */
-int _strlen(const char *s)
-{
-	int i = 0;
-
-	while (*s)
-	{
-		i++;
-		s++;
-	}
-	return (i);
 }
