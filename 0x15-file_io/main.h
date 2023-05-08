16a3c823264272471d4b96ee2d27c122fa105d53
saminstein <saminstein@yahoo.com>
2023-05-08 06:15:37 -0500

    prototypes

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,17 @@
+#ifndef _FILE_IO_
+#define _FILE_IO_
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+
+int _putchar(char c);
+int _strlen(const char *s);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
