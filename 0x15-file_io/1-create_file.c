1da59b6558a3921972fdee00e25edb9a78337109
saminstein <saminstein@yahoo.com>
2023-09-04 08:03:48 +0100

    files

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include "main.h"
+
+
+/**
+ * create_file - function that creates a file
+ * @filename: The name of the file
+ * @text_content: content to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+		write(fd, text_content, strlen(text_content));
+
+	close(fd);
+	return (1);
+}
