e08a90c198c8b45285aae3feffe70cb83ce88a36
saminstein <saminstein@yahoo.com>
2024-03-08 19:07:19 +0100

    ..

--- 0x17-doubly_linked_lists/0-main.c
+++ 0x17-doubly_linked_lists/0-main.c
@@ -12,23 +12,23 @@ int main(void)
 {
 	dlistint_t *head;
 	dlistint_t *new;
-    	dlistint_t hello = {8, NULL, NULL};
+	dlistint_t hello = {8, NULL, NULL};
 	size_t n;
 
 	head = &hello;
-    	new = malloc(sizeof(dlistint_t));
+	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 	{
 		dprintf(2, "Error: Can't malloc\n");
 		return (EXIT_FAILURE);
 	}
 	new->n = 9;
-    	head->prev = new;
-    	new->next = head;
-    	new->prev = NULL;
-    	head = new;
+	head->prev = new;
+	new->next = head;
+	new->prev = NULL;
+	head = new;
 	n = print_dlistint(head);
-    	printf("-> %lu elements\n", n);
-    	free(new);
-    	return (EXIT_SUCCESS);
+	printf("-> %lu elements\n", n);
+	free(new);
+	return (EXIT_SUCCESS);
 }
