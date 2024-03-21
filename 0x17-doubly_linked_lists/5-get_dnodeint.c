fe15471a468cfc9505446c7028440ec75889ad12
Saminstein <saminstein@yahoo.com>
2024-03-21 12:16:20 -0700

    ..

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -11,20 +11,14 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	dlistint_t *current = head;
 	unsigned int position = 0;
 
-  	if (head == NULL && index < 0)
-  	{
-	 	return (NULL);
-  	}	
-
-  	while (current != NULL && index > 0)
-  	{
-		current = current->next;
-		position++;
-
-		if (position == index && current != NULL)
+  	while (current == NULL)
+	{
+		if (position == index)
 		{
 			return (current);
 		}
+		current = current->next;
+		position++;
 	}
 	return (NULL);
 }
