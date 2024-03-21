d363eb20b77f68c1f512d932e84fea0f95fb6c13
Saminstein <saminstein@yahoo.com>
2024-03-21 11:51:34 -0700

    ..

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,13 +8,14 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	int num_of_nodes = 0;
+	size_t num_of_nodes = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		num_of_nodes++;
 		h = h->next;
+		num_of_nodes++;
 	}
 	return (num_of_nodes);
 }
+
