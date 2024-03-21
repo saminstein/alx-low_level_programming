9de8b6d29edc9f327e5d971d0f34c51456060ffa
Saminstein <saminstein@yahoo.com>
2024-03-21 11:17:38 -0700

    ..

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -7,31 +7,32 @@
  * @n: an integer to add to the new node
  * Return: the address of the new element, or NULL if it failed
  */
- dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
- {
-	 dlistint_t *new_node, *current;
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node, *current;
 
-	 new_node = malloc(sizeof(dlistint_t));
-	 if (new_node == NULL)
-		 return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
 
-	 new_node->n = n;
-   	 new_node->prev = NULL;
-   	 new_node->next = NULL;
-
-	 if (*head == NULL)
-	 {
-		 *head = new_node;
-	 }
-	 else
-	 {
-		 current = *head;
-		 while (current->next != NULL)
-		 {
-			 current = current->next;
-		 }
-		 current->next = new_node;
-     		 new_node->prev = current;
-	 }
-	 return new_node;
- }
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		current = *head;
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = new_node;
+		new_node->prev = current;
+	}
+	return (new_node);
+}
