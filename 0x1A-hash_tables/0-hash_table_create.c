22422917712141f46f0ab5a3dda89277a4bab074
Saminstein <saminstein@yahoo.com>
2024-04-13 08:38:21 -0700

    >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,23 +7,23 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-  hash_table_t *new_table;
-  hash_node_t **array;
-  unsigned int i;
-  
-  new_table = malloc(sizeof(hash_table_t));
-  if (new_table == NULL)
-    return NULL;
-    
-  array = malloc(sizeof(hash_node_t *) * size);
-  if (array == NULL)
-    return NULL;
-    
-  for (i = 0; i < size; i++)
-    array[i] = NULL;
+	hash_table_t *new_table;
+	hash_node_t **array;
+	unsigned int i;
 
-  new_table->array = array;
-  new_table->size = size;
-  
-  return (new_table);
+	new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
+		return (NULL);
+
+	array = malloc(sizeof(hash_node_t *) * size);
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		array[i] = NULL;
+
+	new_table->array = array;
+	new_table->size = size;
+
+	return (new_table);
 }
