754965fc3b9fbc7317acd62ff1bcd98d2e8c3980
saminstein <saminstein@yahoo.com>
2024-04-14 01:23:40 +0100

    >>> ht[betty]

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,12 +1,32 @@
 #include "hash_tables.h"
 
 /**
- * function that retrieves a value associated with
- * a key
- * @ht: is the hash table you want to look into
+ * hash_table_get - function that retrieves a
+ * value associated with a key
+ * @ht: is the hash table you want to look int
+ * o
  * @key: the key you are looking for
- * Return: value associated with the element, or N
- * ULL if key couldn’t be found
+ * Return: value associated with the element,o
+ * r NULL if key couldn’t be found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	unsigned long int index;
+	hash_node_t *tmp;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
+	index = key_index((unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			return (tmp->value);
+		}
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
