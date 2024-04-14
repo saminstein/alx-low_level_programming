99c546b9a009f8a852b3e02f15e87d084edf37ba
Saminstein <saminstein@yahoo.com>
2024-04-13 19:25:52 -0700

    >>> ht[betty] = cool

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -26,7 +26,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			free(current->value);
 			current->value = strdup(value);
-			return 1;
+			return (1);
 		}
 		current = current->next;
 	}
