6990e94b4441fca174be6b2347f5257e0ebbe419
saminstein <saminstein@yahoo.com>
2024-06-06 20:08:48 +0100

    island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,32 +3,39 @@
 Module for island perimeter
 """
 
+
 def island_perimeter(grid):
     '''
     Returns the perimeter of the island
 
-    Attribute: 
+    Attribute:
         grid (int): list of list of integers
     '''
 
     perimeter = 0
 
-    # get the num of rows & cols
     rows = len(grid)
     cols = len(grid[0])
 
     for i in range(rows):
         for j in range(cols):
+
+            # check if cell is land
             if grid[i][j] == 1:
+
+                # check up neighbour
                 if i == 0 or grid[i - 1][j] == 0:
                     perimeter += 1
 
+                # check down neighbour
                 if i == i - 1 or grid[i + 1][j] == 0:
                     perimeter += 1
 
+                # check left neighbour
                 if j == 0 or grid[i][j - 1] == 0:
                     perimeter += 1
 
+                # check right neighbour
                 if j == j - 1 or grid[i][j + 1] == 0:
                     perimeter += 1
 
